cf7fb1fdfc92583964efb0bbc59bb81fc6d01cd3
Karagar <balkov_arthur@mail.ru>
2020-10-15 03:36:00 +0300

    alpha version of telnet

--- hw11_telnet_client/main.go
+++ hw11_telnet_client/main.go
@@ -1,6 +1,93 @@
 package main
 
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"time"
+)
+
 func main() {
-	// Place your code here
-	// P.S. Do not rush to throw context down, think think if it is useful with blocking operation?
+	timeoutStr := flag.String("timeout", "10s", "Timeout for connection to server. Default 10s")
+	flag.Parse()
+	timeout, err := time.ParseDuration(*timeoutStr)
+	if err != nil {
+		println(fmt.Errorf("Error while parse timeout duration: %v", err))
+		return
+	}
+	if len(flag.Args()) != 2 {
+		println("Not enought arguments")
+		return
+	}
+	targetAdress := net.JoinHostPort(flag.Args()[0], flag.Args()[1])
+	go func() {
+		handleServer(targetAdress)
+	}()
+	time.Sleep(2 * time.Second)
+	client := NewTelnetClient(targetAdress, timeout, os.Stdin, os.Stdout)
+	client.Connect()
+	go func() {
+		for {
+			client.Send()
+			if err != nil {
+				break
+			}
+		}
+		return
+	}()
+	go func() {
+		for {
+			client.Receive()
+			if err != nil {
+				break
+			}
+		}
+		return
+	}()
+	time.Sleep(300 * time.Second)
+	client.Close()
+}
+
+func handleServer(address string) {
+	l, err := net.Listen("tcp", address)
+	if err != nil {
+		log.Fatalf("Cannot listen: %v", err)
+	}
+	defer l.Close()
+	log.Println("LISTENED")
+
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			log.Fatalf("Cannot accept: %v", err)
+		}
+		log.Println("ACCEPTED")
+		handleConnection(conn)
+	}
+}
+
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
+	conn.Write([]byte(fmt.Sprintf("Welcome to %s, friend from %s\n", conn.LocalAddr(), conn.RemoteAddr())))
+
+	scanner := bufio.NewScanner(conn)
+	for scanner.Scan() {
+		text := scanner.Text()
+		log.Printf("RECEIVED: %s", text)
+		if text == "quit" || text == "exit" {
+			break
+		}
+
+		conn.Write([]byte(fmt.Sprintf("I have received '%s'\n", text)))
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Printf("Error happend on connection with %s: %v", conn.RemoteAddr(), err)
+	}
+
+	log.Printf("Closing connection with %s", conn.RemoteAddr())
+
 }
--- hw11_telnet_client/telnet.go
+++ hw11_telnet_client/telnet.go
@@ -1,17 +1,57 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
 	"io"
+	"net"
 	"time"
 )
 
 type TelnetClient interface {
-	// Place your code here
+	Connect() error
+	Send() error
+	Receive() error
+	Close() error
+}
+
+type TelnetClientObj struct {
+	address string
+	timeout time.Duration
+	in      io.ReadCloser
+	out     io.Writer
+	conn    net.Conn
 }
 
 func NewTelnetClient(address string, timeout time.Duration, in io.ReadCloser, out io.Writer) TelnetClient {
-	// Place your code here
-	return nil
+	return &TelnetClientObj{address, timeout, in, out, nil}
+}
+
+func (tco *TelnetClientObj) Connect() error {
+	conn, err := net.DialTimeout("tcp", tco.address, tco.timeout)
+	tco.conn = conn
+	return err
+}
+
+func (tco *TelnetClientObj) Send() error {
+	return Transfer(tco.in, tco.conn)
+}
+
+func (tco *TelnetClientObj) Receive() error {
+	return Transfer(tco.conn, tco.out)
+}
+
+func (tco *TelnetClientObj) Close() error {
+	return tco.conn.Close()
+}
+
+func Transfer(r io.Reader, w io.Writer) error {
+	scanner := bufio.NewScanner(r)
+	if !scanner.Scan() {
+		return scanner.Err()
+	}
+	_, err := w.Write([]byte(fmt.Sprintf("%s\n", scanner.Text())))
+	return err
 }
 
 // Place your code here
